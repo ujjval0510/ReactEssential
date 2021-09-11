a5893a260b4c0d314c8a5b97ba41b9aa9652d1b4
ujjvalVebuin <ujjvalshrimali@vebuin.com>
2021-09-12 01:03:20 +0900

    React Components example
    React Components example

--- react_essential/src/App.js
+++ react_essential/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Header
+function Header(props) {
+  console.log(props.name);
+  return (
+    <header>
+      <h1>{props.name} Web Developer</h1>
+    </header>
+  )
+}
+
+// Main function
+function Main(props) {
+  return (
+    <section>
+      <p>Services provided by us.</p>
+      <img
+        src="https://github.com/ujjvalVebuin.png"
+        height={300}
+        alt="ujjval github picture"></img>
+      <ul style={{ textAlign: "left" }}>
+        {props.items.map((item) =>
+          <li key={item.id}>{item.title}</li>)
+        }
+      </ul>
+    </section>
+  )
+}
+
+// Footer 
+function FooterArea(props) {
+  return (
+    <footer>
+      <p>Copyright @{props.year}</p>
+    </footer>
+  )
+}
+
+const listProvide = [
+  "Android development",
+  "iOS development",
+  "Web development"
+];
+
+const listObject = listProvide.map((item, i) => ({ id: i, title: item }));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Shrimali" />
+      <Main items={listObject} />
+      <FooterArea year={new Date().getFullYear()} />
     </div>
   );
 }
